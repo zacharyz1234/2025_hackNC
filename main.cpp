ac91ebeacc45bfe8fb31c6150a4b3f411f5cc421
Bryce Hart <bryce.hart23@gmail.com>
2025-10-11 16:09:00 -0400

    fixed main

--- main.cpp
+++ main.cpp
@@ -15,8 +15,8 @@ int main() {
 
         //start draw
         BeginDrawing();
-        ClearBackground((Color{40, 50, 60, 255}));
 
+        HANDLESTATE();
         DrawText("Press [F] to toggle fullscreen", 40, 40, 20, RAYWHITE);
         DrawText("Press [ESC] to quit", 40, 70, 20, RAYWHITE);
 
