a52796390de55b4ec87399082ac8a09cd3c7a86c
Bryce Hart <bryce.hart23@gmail.com>
2025-10-11 18:24:01 -0400

    player changes

--- prim.cpp
+++ prim.cpp
@@ -3,6 +3,19 @@
 
 
 Vector2 updatePlayerPOS(){
-    //player dir here
+    Vector2 r;
+    if(KEY_LEFT){
+        r.x--;
+    }
+    if(KEY_RIGHT){
+        r.x++;
+    }
+    if(KEY_UP){
+        r.y++;
+    }
+    if(KEY_DOWN){
+        r.y--;
+    }
+    return r;
 }
 
