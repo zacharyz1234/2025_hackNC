9a61075fd47aa01e46cabae0e52301ee9fd9c1be
Bryce Hart <bryce.hart23@gmail.com>
2025-10-11 15:47:25 -0400

    Updated gameState.cpp

--- gameState.cpp
+++ gameState.cpp
@@ -2,27 +2,35 @@
 #include"colors.cpp"
 
 
-enum class Gamestate{
+enum Gamestate{
     MAIN_MENU,
     GAME,
     GAME_END,
     
 };
 
-Gamestate state = Gamestate::MAIN_MENU;
+Gamestate state = MAIN_MENU;
+
 
 
 
 
 void HANDLESTATE(){
-    if(state == Gamestate::MAIN_MENU && IsKeyPressed(KEY_ENTER)){ //in main menu
+        switch (state) {
+            case MAIN_MENU:
+                DrawText("MAIN MENU", 200, 150, 40, WHITE);
+                DrawText("PRESS [ENTER] TO START", 180, 250, 20, WHITE);
+                DrawText("PRESS [ESC] TO QUIT", 200, 300, 20, WHITE);
+                break;
 
-    }
-    if(state == Gamestate::GAME){
-        //run game
-    }
-    if(state == Gamestate::GAME_END){
-        //end
-    }
+            case GAME:
+                DrawText("PLAYING GAME...", 180, 200, 30, GREEN);
+                DrawText("PRESS [ESC] TO RETURN TO MENU", 80, 300, 20, WHITE);
+                break;
+
+            case GAME_END:
+                DrawText("GAME OVER", 180, 200, 30, RED);
+                break;
+        }
 }
 
