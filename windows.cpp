0738a9b6b256775843dde5c8fc3e481b8a036471
Bryce Hart <bryce.hart23@gmail.com>
2025-10-11 13:05:59 -0400

    Create windows.cpp

---
+++ windows.cpp
@@ -0,0 +1,19 @@
+#include<prim.cpp>
+
+
+
+
+void windowFunctions(){
+    if (IsKeyPressed(KEY_F)) {
+    fullscreen = !fullscreen;
+
+    if (fullscreen) {
+        SetWindowState(FLAG_FULLSCREEN_MODE);
+    } else {
+        ClearWindowState(FLAG_FULLSCREEN_MODE);
+        SetWindowSize(screenWidth, screenHeight);
+            }
+        }
+}
+
+
