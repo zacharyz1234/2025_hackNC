9e37420594c457fb93ede6eecb82916066a0a583
Bryce Hart <bryce.hart23@gmail.com>
2025-10-11 13:43:18 -0400

    adding gamestate

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include "windows.cpp"
+
+
+
+int main() {
+
+    InitWindow(screenWidth, screenHeight, "Raylib Fullscreen Example");
+    SetTargetFPS(60);
+
+
+    while (!WindowShouldClose()) {
+        windowFunctions();
+
+
+
+        //start draw
+        BeginDrawing();
+        ClearBackground((Color){40, 50, 60, 255});
+
+        DrawText("Press [F] to toggle fullscreen", 40, 40, 20, RAYWHITE);
+        DrawText("Press [ESC] to quit", 40, 70, 20, RAYWHITE);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
+}
