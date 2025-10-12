6c5d64d159f188cea4ba5be98fbceca849cadbe0
zacharyz1234 <zakzeldin@gmail.com>
2025-10-12 03:28:39 -0400

    Added the screen overlay back and the menu is now aligned once more

--- gameState.cpp
+++ gameState.cpp
@@ -77,10 +77,10 @@ void gameRunning(){
 void drawMenu(){
     //DrawText("Press [F] to toggle fullscreen", 40, 40, 20, RAYWHITE);
     //DrawText("Press [ESC] to quit", 70, 70, 20, RAYWHITE);
-    DrawText("MAIN MENU", 345, 150, 40, WHITE);
-    DrawRectangleLines(335, 140, 253, 50, RED);
-    DrawText("PRESS [ENTER] TO START", 320, 250, 20, WHITE);
-    DrawText("PRESS [ESC] TO QUIT", 345, 300, 20, WHITE);
+    DrawRectangle(740, 300, 430, 65, GRAY);
+    DrawText("MAIN MENU", 750, 300, 70, WHITE);
+    DrawText("PRESS [ENTER] TO START", 815, 380, 20, WHITE);
+    DrawText("PRESS [ESC] TO QUIT", 845, 425, 20, WHITE);
 }
 
 
