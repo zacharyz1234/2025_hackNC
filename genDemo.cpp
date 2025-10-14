db8ca00687f1b941fa99f44a974a4dc3723db817
Bryce Hart <bryce.hart23@gmail.com>
2025-10-14 11:11:54 -0400

    Create genDemo.cpp check it out

---
+++ genDemo.cpp
@@ -0,0 +1,129 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<ctime>
+#include<cstdlib>
+/*
+
+
+
+*/
+
+
+
+
+
+
+struct monster{
+
+};
+
+
+struct room {
+    bool init = false;
+    int roomID;
+    bool isPlayerIn = false;
+    bool hasPlayerVisted = false;
+    std::string floorType;
+    std::string wallType;
+    std::vector<monster> monsterNumber;
+    std::string torchColor;
+    int toLeft = -1;
+    int toRight = -1;
+    int toUP = -1;
+    int toDown = -1;
+};
+
+int globalRoomIdCounter = 0;
+std::vector<room> globalRoomVector;
+int globalMaxMonsterSpawn = 1;
+int globalPlayerIsInThisRoom = 0;
+
+
+/*
+When finished, this will make a new vector of completely random monsters
+*/
+std::vector<monster> makeNewMonstersVector(int amount){
+for(int i = 0; i < amount; i++){
+    //push_back monster
+}
+
+}
+
+
+/*
+Generates and returns a new room
+*/
+room generateNewRoom(){
+    std::string floorTypes[6] = {"dirt", "gravel", "glass", "sand", "etc", "etc"};
+    std::string wallTypes[3] = {"wall1", "wall2", "wall3"}; 
+    std::string torchColor[5] = {"blue", "green", "purple", "red", "none"};
+    room r;
+    srand(time(0));
+    int floor = (rand() % 6) + 1;
+    int wall = (rand() % 3) + 1;
+    int monsterCount = (rand() % globalMaxMonsterSpawn) + 1;
+    int torch = (rand() % 5) + 1;
+    r.roomID = globalRoomIdCounter;
+
+    r.torchColor = torchColor[torch];
+    r.floorType = floorTypes[floor];
+    r.wallType = wallTypes[wall];
+    r.monsterNumber = makeNewMonstersVector(monsterCount);
+    
+
+
+
+
+
+    globalRoomIdCounter++; //update count as only one room is generated, and a call on this function will increment the count
+    return r;
+}
+
+
+/*
+checks if the room to the side of room x exist, and if they dont generates a new room and pushes it onto the vector
+in place newroom.ID. SHOULD ONLY BE CALLED ON THE USERS CURRENT ROOM
+*/
+void checkIfRoomsAreLoadedAround(room &AroundThisRoom){
+    if(AroundThisRoom.toLeft == -1){
+        room newRoom = generateNewRoom();
+        AroundThisRoom.toLeft = newRoom.roomID; //set side to that rooms Id
+        globalRoomVector.push_back(newRoom);
+    }
+    if(AroundThisRoom.toRight == -1){
+        room newRoom = generateNewRoom();
+        AroundThisRoom.toRight = newRoom.roomID; //set side to that rooms Id
+        globalRoomVector.push_back(newRoom);
+    }
+    if(AroundThisRoom.toUP == -1){
+        room newRoom = generateNewRoom();
+        AroundThisRoom.toUP = newRoom.roomID; //set side to that rooms Id     
+        globalRoomVector.push_back(newRoom);
+    }
+    if(AroundThisRoom.toDown == -1){
+        room newRoom = generateNewRoom();
+        AroundThisRoom.toDown = newRoom.roomID; //set side to that rooms Id 
+        globalRoomVector.push_back(newRoom);    
+    }
+    }
+    
+
+
+
+
+
+int main(){
+    bool gamerunning = true;
+
+
+
+    char temp = 'q'; //til finished
+    globalRoomVector.push_back(generateNewRoom()); //makes first room before window opens
+    while(temp != 'q'){
+        checkIfRoomsAreLoadedAround(globalRoomVector[globalPlayerIsInThisRoom]); //global playerIsInThisRoom should always update with player
+
+    }
+
+
+}
